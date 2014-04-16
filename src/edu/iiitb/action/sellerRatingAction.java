d0fea0e5c456360f32df5d6c0f7de1d4c8888ac5
parasmittal <paras.8december@gmail.com>
2014-04-16 06:14:52 +0530

    Seller Rating & review page

--- src/edu/iiitb/action/sellerRatingAction.java
+++ src/edu/iiitb/action/sellerRatingAction.java
@@ -1,8 +1,73 @@
 package edu.iiitb.action;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import edu.iiitb.database.DBHandlerForSellerRating;
+import edu.iiitb.model.SellerRating;
+import edu.iiitb.model.SellerReviews;
+
 public class sellerRatingAction {
-public String execute() {
-	return "success";
+
+	int sellerId ;
+	ArrayList<SellerReviews> reviews ;
+	SellerRating rating ;
 	
-}
+	
+	public int getSellerId() {
+		return sellerId;
+	}
+
+
+
+	public void setSellerId(int sellerId) {
+		this.sellerId = sellerId;
+	}
+
+	
+
+	public ArrayList<SellerReviews> getReviews() {
+		return reviews;
+	}
+
+
+
+	public void setReviews(ArrayList<SellerReviews> reviews) {
+		this.reviews = reviews;
+	}
+
+	
+
+	public SellerRating getRating() {
+		return rating;
+	}
+
+
+
+	public void setRating(SellerRating rating) {
+		this.rating = rating;
+	}
+
+
+
+	public String execute() {
+
+		reviews = new ArrayList<SellerReviews>();
+		rating = new SellerRating();
+		
+		DBHandlerForSellerRating dbHandler = new DBHandlerForSellerRating();
+		
+		try {
+			dbHandler.fetchReviewsForSeller(sellerId , reviews);
+			dbHandler.fetchRatingForSeller(sellerId , rating);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Exception at execute() of sellerRatingAction");
+			e.printStackTrace();
+			return "error";
+		}
+		
+		return "success";
+
+	}
 }
---
+++ src/edu/iiitb/database/DBHandlerForSellerRating.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package edu.iiitb.database;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.mysql.jdbc.Connection;
+
+import edu.iiitb.model.SellerRating;
+import edu.iiitb.model.SellerReviews;
+
+/**
+ * @author paras
+ *
+ */
+public class DBHandlerForSellerRating {
+
+	DBConnectivity db=new DBConnectivity();
+	String query;
+	
+	public void fetchReviewsForSeller(int sellerId,
+			ArrayList<SellerReviews> reviews) throws SQLException {
+		// TODO Auto-generated method stub
+		Connection con = db.createConnection();
+		query = "select concat(UC.firstName , ' ' , UC.lastName) name , RR.review from FlipKartDatabase.ReviewNRating RR Inner join FlipKartDatabase.UserCredantials UC "
+				+ "on RR.userId = UC.userId where RR.productSellerId = "+sellerId+"";
+		ResultSet rs=db.executeQuery(query, con);
+		while(rs.next())
+		{
+			SellerReviews model = new SellerReviews();
+			model.setUserName(rs.getString(1));
+			model.setReview(rs.getString(2));
+			reviews.add(model);
+		}
+		con.close();
+	}
+
+	public void fetchRatingForSeller(int sellerId, SellerRating rating) throws SQLException {
+		// TODO Auto-generated method stub
+		
+		Connection con = db.createConnection();
+		Connection con1 = db.createConnection();
+		query = "select count(*) , concat(UC.firstName , ' ' , UC.lastName) name , S.description from "
+				+ "FlipKartDatabase.ReviewNRating RR Inner join FlipKartDatabase.Seller S"
+				+ " on RR.productSellerId = S.sellerId Inner join FlipKartDatabase.UserCredantials UC "
+				+ "on S.userId = UC.userId where RR.productSellerId = "+sellerId+"";
+		ResultSet rs=db.executeQuery(query, con);
+		if(rs.next())
+		{
+			rating.setTotalReview(rs.getInt(1));
+			rating.setSellerName(rs.getString(2));
+			rating.setDescription(rs.getString(3));
+		}
+		con.close();
+		query = "select count(*) , rating from FlipKartDatabase.ReviewNRating "
+				+ "where productSellerId = "+sellerId+" group by rating";
+		ResultSet rs1=db.executeQuery(query, con1);
+		while(rs1.next())
+		{
+			if(rs1.getString(2).equalsIgnoreCase("1"))
+				rating.setOneStar((rs1.getInt(1) * 100 )/rating.getTotalReview());
+			else if(rs1.getString(2).equalsIgnoreCase("2"))
+				rating.setTwoStar((rs1.getInt(1) * 100 )/rating.getTotalReview());
+			else if(rs1.getString(2).equalsIgnoreCase("3"))
+				rating.setThreeStar((rs1.getInt(1) * 100 )/rating.getTotalReview());
+			else if(rs1.getString(2).equalsIgnoreCase("4"))
+				rating.setFourStar((rs1.getInt(1) * 100 )/rating.getTotalReview());
+			else 
+				rating.setFiveStar((rs1.getInt(1) * 100 )/rating.getTotalReview());
+		}
+		con1.close();
+	}
+	
+	
+	
+	
+}
