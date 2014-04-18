446fee4381d43553aac04a935f3c42183146e151
M@verick <deveshsingh.88@gmail.com>
2014-04-18 22:58:22 +0530

    Place order and payment

---
+++ src/edu/iiitb/action/cashOnDeliveryOderPayment.java
@@ -0,0 +1,56 @@
+package edu.iiitb.action;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import edu.iiitb.database.DBHandlerForUser;
+
+public class cashOnDeliveryOderPayment  extends ActionSupport implements SessionAware , ServletRequestAware , ServletResponseAware
+{
+	private static final long serialVersionUID = 1L;
+	private HttpServletResponse servletResponse;
+	private HttpServletRequest servletRequest;
+	private Map session;
+	
+	public HttpServletResponse getServletResponse() {
+		return servletResponse;
+	}
+	public void setServletResponse(HttpServletResponse servletResponse) {
+		this.servletResponse = servletResponse;
+	}
+	public HttpServletRequest getServletRequest() {
+		return servletRequest;
+	}
+	public void setServletRequest(HttpServletRequest servletRequest) {
+		this.servletRequest = servletRequest;
+	}
+	public Map getSession() {
+		return session;
+	}
+	public void setSession(Map session) {
+		this.session = session;
+	}
+	
+	public String execute() throws SQLException
+	{
+		DBHandlerForUser db = new DBHandlerForUser();		
+		String orderId =  String.valueOf(session.get("orderId"));
+		String grandTotal = String.valueOf(session.get("grandTotal"));
+		//String bankName = String.valueOf(session.get("bankName"));
+		String bankName = "Flipkart";
+		String paymentType = "Cash On Delivery";		
+		db.insertOrderPaymentDetails(orderId, bankName, grandTotal, paymentType);
+		db.updateProductQuantityAfterPurchase(orderId);			
+		return "success";
+	}
+
+}
